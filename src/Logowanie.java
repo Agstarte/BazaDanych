ddd4c5337df4131a83f30767b9626d7807065837
Jacuq <51338734+Jacuq@users.noreply.github.com>
2019-06-04 19:33:08 +0200

    Poprawa logowania

--- src/Logowanie.java
+++ src/Logowanie.java
@@ -4,23 +4,41 @@ import java.util.Scanner;
 
 public class Logowanie implements ILogowanie{
 
-    public static int login (String log, String ha)
+    public static int login ()
     {
-        try
-        {
-            FileReader fr = new FileReader("loginy.txt");
-            BufferedReader in = new BufferedReader(fr);
+        String login, haslo;
+        Scanner scan = new Scanner(System.in);
 
-            String l = in.readLine();
-            String h = in.readLine();
-            if(l.equals(log) && h.equals(ha))
-                return 1;
 
-        }
-        catch(IOException e)
-        {
-            System.out.println("Nie udalo sie sprawdzic loginu");
-        }
+
+        String temp1; //linijka z pliku
+        String[] temp2 = {"",""}; //linijka z pliku po podziale na User/admin, login,haslo
+
+        int fails =0;
+        do {
+            System.out.print("Login:");
+            login = scan.nextLine();
+            System.out.print("Haslo:");
+            haslo = scan.nextLine();
+            try {
+                FileReader fr = new FileReader("loginy.txt");
+                BufferedReader in = new BufferedReader(fr);
+                while ((temp1 = in.readLine()) != null) {
+                    temp2 = temp1.split("-");
+                    if (login.equals(temp2[1]) && haslo.equals(temp2[2])) {
+                        if (temp2[0].equals("A")) //jesli to konto admina to zwraca 1
+                            return 1;
+                        else
+                            return 2;
+                    }
+
+                }
+                fails++;
+                System.out.println("Niepoprawny login lub haslo, sprobuj ponownie \nPozostale proby:" + (5-fails));
+            } catch (IOException e) {
+                System.out.println("Nie udalo sie sprawdzic loginu");
+            }
+        }while(fails<5);
         return 0;
     }
 
@@ -28,7 +46,7 @@ public class Logowanie implements ILogowanie{
     public void Loguj ()
     {
 
-        int admin = 1;
+        int admin = login();
         if (admin == 0) {
             System.out.println("nie masz uprawnien do korzystania z programu");
         } else {
@@ -108,14 +126,15 @@ public class Logowanie implements ILogowanie{
                         System.out.println("\nKliknij enter zeby przejsc do menu");
                         enterek();
                         break;
-                    case "0": break;
+                    case "0":
+                        break;
                     default:
                         System.out.println("Niepoprawny wybor");
                         System.out.println("\nKliknij enter zeby przejsc do menu");
                         enterek();
                         break;
                 }
-            } while (wybor != "0");
+            } while (!wybor.equals("0"));
             testowa.zapiszBaze();
         }
     }
